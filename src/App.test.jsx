afd497c423909b0da0b78fc24650351f151faef2
BrendenSeifried <brendenseifried@gmail.com>
2022-04-25 16:42:23 -0700

    done with app.test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,17 @@ import { render, screen } from '@testing-library/react'
 
 import App from "./App"
 
-test('Should render the header', () => {
-    // render(<App />)
-    // screen.debug();
+describe('App', () => {
+it('Should render the header', async () => {
+    render(<App />);
+    // screen.getByAltText('Alchemy Logo');
+    // screen.getByText(/loading.../i);
+    const school = screen.getByAltText('Alchemy Logo');
+    expect(school).toBeInTheDocument();
+    const name = await screen.findByText('Vonta');
+    expect(name).toBeInTheDocument();
 
-    // screen.get
+   
+    // screen.debug();
+})
 })
