d917aef75a186e80633b9ce715056c024233a21e
BrendenSeifried <brendenseifried@gmail.com>
2022-04-25 18:05:25 -0700

    entered loading back in and added stuff from Dan the banner part so no extra credit needed

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,13 +7,25 @@ describe('App', () => {
 it('Should render the header', async () => {
     render(<App />);
     // screen.getByAltText('Alchemy Logo');
-    // screen.getByText(/loading.../i);
+    screen.getByText(/loading.../i);
+    
     const school = screen.getByAltText('Alchemy Logo');
     expect(school).toBeInTheDocument();
+
+
     const name = await screen.findByText('Vonta');
     expect(name).toBeInTheDocument();
 
+
+
+    // how to grab banner color
+    const header = screen.getByRole('banner');
+    // console.log('header', header.innerHTML); check if it grabs the header
+    expect(header).toHaveStyle({
+        background: 'var(---grey)',
+    })
+    /// ////////
    
-    // screen.debug();
+    screen.debug();
 })
 })
